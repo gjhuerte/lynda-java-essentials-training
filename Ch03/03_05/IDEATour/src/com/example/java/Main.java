c96891caa03a341b1661b1bffea79eb3ca737749
Gabriel Jay Huerte <gjhuerte@gmail.com>
2018-08-22 19:41:06 +0800

    Importing java project on IDEA

---
+++ Ch03/03_05/IDEATour/src/com/example/java/Main.java
@@ -0,0 +1,10 @@
+package com.example.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from IDEA!");
+        String aString = "David";
+        System.out.println("Your name is: " + aString);
+    }
+}
