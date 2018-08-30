8faa6849ab5a1cfd31a15773481797a38530c80d
Gabriel Jay Huerte <gjhuerte@gmail.com>
2018-08-30 14:54:26 +0800

    Characters tutorials in Java

---
+++ Ch04/04_07/Characters/src/com/example/java/Main.java
@@ -0,0 +1,26 @@
+package com.example.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char c1 = '1';
+        char c2 = '2';
+        char c3 = '3';
+
+        System.out.println("Char 1: " + c1);
+        System.out.println("Char 2: " + c2);
+        System.out.println("Char 3: " + c3);
+
+        char dollarSign = '\u0024';
+        System.out.println("Currency: " + dollarSign);
+
+        char a1 = 'a';
+        char a2 = 'b';
+        char a3 = 'c';
+        System.out.print(Character.toUpperCase(a1));
+        System.out.print(Character.toUpperCase(a2));
+        System.out.println(Character.toUpperCase(a3));
+
+    }
+}
