e7dc21a9131fc10dc965d6555d411cef66573fb0
Gabriel Jay Huerte <gjhuerte@gmail.com>
2018-08-31 18:29:37 +0800

    Debugging tutorials on Java

---
+++ Ch06/06_02/Debugging/src/com/example/java/Backup.java
@@ -0,0 +1,20 @@
+package com.example.java;
+
+/**
+ * Created by Gabriel on 31/08/2018.
+ * Copy all the contents in the backup
+ * class to the main class
+ */
+public class Backup {
+
+    public static void main(String[] args) {
+
+        String welcome = "Welcome!";
+        char[] chars = welcome.toCharArray();
+
+        for (int i = 0; i < chars.length; i++) {
+            System.out.println(chars[i]);
+        }
+
+    }
+}
