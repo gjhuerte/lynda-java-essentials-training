53d95d39c8fedea2b26311c21427a4ed63821081
Gabriel Jay Huerte <gjhuerte@gmail.com>
2018-09-02 14:29:12 +0800

    Finished tutorials on passing arguments in Java

--- Ch07/07_06/PassingArguments/src/com/example/java/Main.java
+++ Ch07/07_06/PassingArguments/src/com/example/java/Main.java
@@ -3,14 +3,29 @@ package com.example.java;
 public class Main {
 
     public static void main(String[] args) {
+//        primitiveValuesWrappedInObjects();
+//        passingPrimitiveValues();
+        objectVariablesAsReference();
+    }
+
+    public static void objectVariablesAsReference() {
+        String original = "Original!";
+        System.out.println("Original before: " + original);
+        changeString(original);
+        System.out.println("Original after: " + original);
+    }
+
+    public static void changeString(String inMethod) {
+        inMethod = "New!";
+        System.out.println("In method: " + inMethod);
     }
 
     public static void increment(int[] inMethod) {
-        inMethod[0]++;
+        inMethod[0] ++;
         System.out.println("In method: " + inMethod[0]);
     }
 
-    public static void primitiveValuesWrappedinObjects() {
+    public static void primitiveValuesWrappedInObjects() {
         int[] original = {10, 20, 30};
         System.out.println("Original Value: " + original[0]);
         increment(original);
@@ -25,7 +40,7 @@ public class Main {
     }
 
     public static void increment(int inMethod) {
-        inMethod++;
+        inMethod ++;
         System.out.println("In method: " + inMethod);
     }
 }
