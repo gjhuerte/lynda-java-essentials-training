773dcc3c9ae39dac107a6dc511a387d509bd9cc4
Gabriel Jay Huerte <gjhuerte@gmail.com>
2018-09-04 19:22:11 +0800

    Extending classes and method overriding project

---
+++ Ch10/10_02/ExtendingClasses/src/com/example/java/Main.java
@@ -0,0 +1,25 @@
+package com.example.java;
+
+import com.example.java.model.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Olive> olives = new ArrayList<>();
+        olives.add(new Kalamata());
+        olives.add(new Kalamata());
+        olives.add(new Ligurian());
+        olives.add(new Ligurian());
+        olives.add(new Ligurian());
+        olives.add(new Ligurian());
+
+        OlivePress press = new OlivePress();
+        int totalOil =  press.getOil(olives);
+        System.out.println("Total olive oil: " + totalOil);
+
+    }
+}
---
+++ Ch10/10_02/ExtendingClasses/src/com/example/java/model/Olive.java
@@ -0,0 +1,39 @@
+package com.example.java.model;
+
+public class Olive {
+
+    private OliveName name = OliveName.KALAMATA;
+    private OliveColor color = OliveColor.PURPLE;
+    private int oil = 3;
+
+    public Olive(OliveName name, OliveColor color, int oil) {
+        this.name = name;
+        this.color = color;
+        this.oil = oil;
+    }
+
+    public OliveName getName() {
+        return name;
+    }
+
+    public void setName(OliveName name) {
+        this.name = name;
+    }
+
+    public OliveColor getColor() {
+        return color;
+    }
+
+    public void setColor(OliveColor color) {
+        this.color = color;
+    }
+
+    public int crush() {
+        System.out.println("Ouch!");
+        return oil;
+    }
+
+    public void setOil(int oil) {
+        this.oil = oil;
+    }
+}
