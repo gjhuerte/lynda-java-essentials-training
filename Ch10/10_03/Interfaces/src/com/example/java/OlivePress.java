3ec11fffc8a829051cccc0e37dc960912e99ef51
Gabriel Jay Huerte <gjhuerte@gmail.com>
2018-09-04 19:46:33 +0800

    Implementing interfaces in Java

---
+++ Ch10/10_03/Interfaces/src/com/example/java/OlivePress.java
@@ -0,0 +1,28 @@
+package com.example.java;
+
+import com.example.java.model.Olive;
+
+import java.util.List;
+
+public class OlivePress implements Press {
+
+    private int currentOil;
+
+    public int getOil(List<Olive> olives) {
+
+        int totalOil = currentOil;
+
+        for (Olive o : olives) {
+            System.out.println(o.getName());
+            totalOil += o.crush();
+        }
+
+        return totalOil;
+    }
+
+    @Override
+    public void setOil(int oil) {
+        currentOil = oil;
+    }
+
+}
---
+++ Ch10/10_03/Interfaces/src/com/example/java/model/OliveColor.java
@@ -0,0 +1,11 @@
+package com.example.java.model;
+
+public enum OliveColor {
+    PURPLE(0x2E0854), BLACK(0x000000);
+
+    public long color;
+
+    OliveColor(long color) {
+        this.color = color;
+    }
+}
